1dd0ecd64fba7facf3bebfe04a9bfe0032c2c8b7
atm951999 <amartin3@woh.rr.com>
2021-12-18 04:33:16 -0500

    Unbelievable

--- src/app/pages/invoice-dialog/invoice-dialog.component.ts
+++ src/app/pages/invoice-dialog/invoice-dialog.component.ts
@@ -7,6 +7,7 @@ import { IService } from '../../models/services.interface';
 import { HttpClient } from '@angular/common/http';
 import { Invoice } from 'src/app/models/invoice';
 import { Observable } from 'rxjs';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-invoice-dialog',
@@ -17,11 +18,9 @@ export class InvoiceDialogComponent implements OnInit {
 
   date : string;
   totalCharge : number;
-  Invoice: [{
+  invoice: Invoice;
 
-  }]
-
-  constructor(private http: HttpClient, private datePipe: DatePipe, @Inject(MAT_DIALOG_DATA) public data: { services: IService[],
+  constructor(private cookieService: CookieService, private http: HttpClient, private datePipe: DatePipe, @Inject(MAT_DIALOG_DATA) public data: { services: IService[],
     laborHours: number,
     partsAmount: number
   }) {
@@ -33,22 +32,28 @@ export class InvoiceDialogComponent implements OnInit {
     return this.data.services.filter(service => service.selected);
   }
 
-  createInvoice(userName: string, invoice: Invoice): Observable<any> {
-    return this.http.post(`/api/invoice/${userName}`, {
-        userName : userName,
-        lineItems: invoice.getLineItems(),
-        partsAmount: invoice.partsAmount,
-        laborAmount: invoice.getLaborAmount(),
-        lineItemTotal: invoice.getLineItemTotal(),
-        total: invoice.getTotal()
-    })
+  createInvoice(): void {
+    this.http.post(`/api/invoice/${this.invoice.getUserName()}`, {
+        userName : this.invoice.getUserName(),
+        lineItems: this.invoice.getLineItems(),
+        partsAmount: this.data.partsAmount,
+        laborAmount: this.data.laborHours*50,
+        lineItemTotal: this.invoice.getLineItemTotal(),
+        total: this.totalCharge
+    }).subscribe()
 }
 
   ngOnInit(): void {
     let total = this.data.partsAmount;
+    this.invoice = new Invoice(this.cookieService.get("session_user"));
     total = total + (this.data.laborHours * 50);
     this.getSelectedServices().forEach(service => {
       total = total + service.value;
+      this.invoice.getLineItems().push({
+        title: service.name,
+        price: service.value
+      })
+
     });
     this.totalCharge = total;
   }
